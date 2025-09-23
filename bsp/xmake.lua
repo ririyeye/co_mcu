8db54a5636ad0c0c98d5a11943a7667df9a40f05
ririyeye <200610237@qq.com>
2025-09-23 22:36:16 +0800

    feat: 添加 GD32 LED 测试示例编译选项

--- bsp/xmake.lua
+++ bsp/xmake.lua
@@ -6,6 +6,14 @@ option("gd32")
     set_description("Enable building GD32 firmware libraries")
 option_end()
 
+-- 选项：是否编译 gd32 led 测试示例（依赖 gd32）
+option("gd32_test")
+    add_deps("gd32")
+    set_default(false)
+    set_showmenu(true)
+    set_description("Build GD32 LED test example")
+option_end()
+
 -- 按需包含 gd32 工程
 if has_config("gd32") then
     includes("gd32e11")
